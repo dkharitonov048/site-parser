75a3628f873b46f381cfe0b2edc8e32a3bbfffe2
Dmitriy <DKharitonov@outlook.com>
2018-10-20 16:21:59 +0700

    Add project files.

---
+++ SiteParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SiteParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
